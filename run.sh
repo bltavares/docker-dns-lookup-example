c93382b17b06ecefd996fe043d3a889b33fef160
Bruno Tavares <bruno.exz@gmail.com>
2015-06-02 15:53:08 +0200

    Include a bind server so you can do the lookup

--- run.sh
+++ run.sh
@@ -5,22 +5,26 @@
 docker build -t server-lookup -f server.dockerfile .
 docker build -t client-lookup -f client.dockerfile .
 
+file_path=${file_path:-$(pwd)}
 # Start two servers, a staging one and a production one
 
-docker run -d -h='server.example.com' -v $(pwd)/production.index.html:/var/data/index.html --name prod server-lookup
-docker run -d -h='server.staging.example.com' -v $(pwd)/staging.index.html:/var/data/index.html --name staging server-lookup
+docker run -d -h='server.example.com' -v $file_path/production.index.html:/var/data/index.html --name prod server-lookup
+docker run -d -h='server.staging.example.com' -v $file_path/staging.index.html:/var/data/index.html --name staging server-lookup
+
+# Start a dns container
+docker run --name='bind' -d -p 53/udp -v $file_path/bind:/data sameersbn/bind:latest
 
 # Get the ip for those containers
 
 prod_ip=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' prod)
 staging_ip=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' staging)
+bind_ip=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' bind)
 
 # Start a client with a fake dns registred for those
 
 docker run \
   --rm \
-  --add-host=server.example.com:$prod_ip \
-  --add-host=server.staging.example.com:$staging_ip \
   --hostname=client.example.com \
+  --dns=$bind_ip \
   --dns-search=. \
   -ti client-lookup bash
